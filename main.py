ec756045e868d5d1372d5cb12458306e14073f20
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2023-02-21 21:26:15 +0600

    сделал Декоратор и FizzBuzz

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-print("Hello world")
-#4ernovik
+from datetime import datetime
+from functools import wraps
+
+def timer(func):
+    @wraps(func)
+    def time_zone(*args, **kwargs):
+        start = datetime.now()
+        func(*args, **kwargs)
+        end = datetime.now()
+        res = end - start
+        return print(f'{func(*args, **kwargs)}\nВремя выполнения: {res}')
+    return time_zone
+
+        
+
+@timer
+def fizz_buzz(num):
+    for x in num:
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print('Buzz')
+        else:
+            print(x)
+            
+fizz_buzz = fizz_buzz(range(1, 101))
