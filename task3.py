37b8ef32d4366e961f29cdafd347fe4086b0aece
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2023-02-25 14:41:37 +0600

    Добавил еще одно решение

--- task3.py
+++ task3.py
@@ -1,5 +1,11 @@
-n = int(input('Enter num: '))
-a = []
-for i in range(1, n+1):
-    a.append(i)
-print(sum(a))
+# #1
+# n = int(input('Enter num: '))
+# a = []
+# for i in range(1, n+1):
+#     a.append(i)
+# print(sum(a))
+
+#2
+for i in range(-100, 100):
+    if i < 0:
+        print(i)
